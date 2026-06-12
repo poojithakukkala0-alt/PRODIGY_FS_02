af551bca91f5a7a54df575b4c619f70e67c2af5f
poojithakukkala0-alt <poojithakukkala0-alt@example.com>
2026-06-12 15:59:42 +0530

    chore: redirect api url to live render server

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://prodigy-fs-02-p8oj.onrender.com' });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
